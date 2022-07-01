7a2101bda2fdcb572a76d288a5ce04c10928b2e3
alwinchoi <alwinchoi2002@gmail.com>
2022-07-01 16:27:05 +0800

    changing to to do list page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import ToDoList from './Component/ToDoList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> 
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <ToDoList />
     </div>
   );
 }
---
+++ src/Component/ToDoList.js
@@ -0,0 +1,9 @@
+import "../css/ToDoList.css"
+
+const ToDoList = props => {
+    return (
+        <p>To Do list</p>
+    )
+}
+
+export default ToDoList;
