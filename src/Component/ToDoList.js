9a5e8ff770042f99711730292f7422db29219769
alwinchoi <alwinchoi2002@gmail.com>
2022-07-01 17:06:59 +0800

    fixed title

--- src/Component/ToDoList.js
+++ src/Component/ToDoList.js
@@ -1,8 +1,13 @@
 import "../css/ToDoList.css"
 
+import React from "react";
+
 const ToDoList = props => {
+    const current = new Date()
+    const date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`
+
     return (
-        <p>To Do list</p>
+        <p className="title">To-do list - {date}</p>
     )
 }
 
